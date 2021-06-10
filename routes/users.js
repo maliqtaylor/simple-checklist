113dd5c31acc9037e8bc2ad6e8bc05826eafb339
maliqtaylor <maliq.taylor@icloud.com>
2021-06-10 16:45:23 -0400

    initialize auth & user routes

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../controllers/users');
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(require("../config/auth"));
+router.put('/:id', checkAuth, usersCtrl.update)
+router.get('/:id', checkAuth, usersCtrl.profile)
+
+/*---------- Auth Checker ----------*/
+function checkAuth(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({ msg: 'Not Authorized' });
+}
+
+module.exports = router;
